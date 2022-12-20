8abd823c09c93d86ac5f343f98b2392a557f1c32
Maksym Puzanov <puzanov.maksym@gmail.com>
2022-12-20 14:02:50 +0200

    add account form and js logic

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import { isWebp, toggleClass } from "./modules/functions.js";
 import * as dynamicAdaptive from './modules/dynamicAdapt.js';
 import * as select from './modules/select.js';
 import * as burger from './modules/burgerMenu.js';
+import * as accountMenu from './components/accountMenu.js'
 
 isWebp();
 
