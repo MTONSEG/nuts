e2f1c82391109260dc683bab86910b3a37c1790e
Maksym Puzanov <puzanov.maksym@gmail.com>
2022-12-23 15:12:40 +0200

    add cart page

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,28 @@ import * as burger from './modules/burgerMenu.js';
 import * as cartPopup from './modules/cartPopup.js';
 
 isWebp();
+
+
+// Форма колличества товаров
+const quantityBody = document.querySelectorAll('.quantity');
+
+if (quantityBody) {
+	for (let elem of quantityBody) {
+		let input = elem.querySelector('.quantity__input');
+
+		elem.addEventListener('click', e => {
+			let onClickMinus = e.target.closest('._minus');
+			let onClickPlus = e.target.closest('._plus');
+
+			if (onClickMinus) {
+				input.value--;
+			} else if (onClickPlus) {
+				input.value++;
+			}
+
+			if (input.value <= 1) {
+				input.value = 1;
+			}
+		})
+	}
+}
